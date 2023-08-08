13169675abdb50478eba89e565872bb7121540c8
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-08-08 19:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,15 +8,3 @@ function updateWindowSize() {
 updateWindowSize();
 window.addEventListener('resize' , updateWindowSize);
 
-// function updateWindowSize() {
-//     const sizeElement = document.querySelector('#sizeInfo h1');
-//     const width = window.innerWidth;
-//     const height = window.innerHeight;
-//     sizeElement.textContent = `Width: ${width} and Height: ${height}`;
-// }
-
-// // Call the updateWindowSize function initially
-// updateWindowSize();
-
-// Update the size whenever the window is resized
-window.addEventListener('resize', updateWindowSize);
